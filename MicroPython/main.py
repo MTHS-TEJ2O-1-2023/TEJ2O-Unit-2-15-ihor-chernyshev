e72fb0aa213019e5eb85298b236136bfe215558f
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-16 17:20:31 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -16,32 +16,26 @@ while True:
         loop_counter = 0
         loop_counter1 = 0
         display.clear()
-
         while loop_counter <= 4:
             display.set_pixel(loop_counter, 0, 9)
             sleep(500)
             display.set_pixel(loop_counter, 0, 0)
             loop_counter = loop_counter + 1
-            
         while loop_counter1 <= 4:
             display.set_pixel(4, loop_counter1, 9)
             sleep(500)
             display.set_pixel(4, loop_counter1, 0)
             loop_counter1 = loop_counter1 + 1
-            
         loop_counter = 4
         loop_counter1 = 4
-        
         while loop_counter >= 0:
             display.set_pixel(loop_counter, 4, 9)
             sleep(500)
             display.set_pixel(loop_counter, 4, 0)
             loop_counter = loop_counter - 1
-            
         while loop_counter1 >= 0:
             display.set_pixel(0, loop_counter1, 9)
             sleep(500)
             display.set_pixel(0, loop_counter1, 0)
             loop_counter1 = loop_counter1 - 1
-            
         display.show(Image.HAPPY)

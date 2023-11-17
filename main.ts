732893311eb55ff49dc4b631d4e77c0e6fa1052c
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-11-17 12:57:17 -0500

    typescript version

--- main.ts
+++ main.ts
@@ -8,6 +8,7 @@
 // variables
 let sprite: game.LedSprite = null
 let loopCounter = 0
+let loopCounter1 = 0
 
 // setup
 basic.clearScreen()
@@ -18,34 +19,35 @@ input.onButtonPressed(Button.A, function () {
   // setup
   basic.clearScreen()
   loopCounter = 0
+  loopCounter1 = 0
   sprite = game.createSprite(0, 0)
-  while (loopCounter <= 4) {
+  // goes from upper right corner to lower left corner
+  while (loopCounter <= 5) {
     basic.pause(500)
-    sprite.move(1)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
     loopCounter = loopCounter + 1
+    // goes from left to right
+    while (loopCounter1 <= 4) {
+      basic.pause(500)
+      sprite.set(LedSpriteProperty.X, loopCounter1)
+      loopCounter1 = loopCounter1 + 1
+    }
   }
-  loopCounter = 0
-  sprite.turn(Direction.Right, 90)
-  while (loopCounter <= 4) {
-    basic.pause(500)
-    sprite.move(1)
-    loopCounter = loopCounter + 1
-  }
-  sprite.turn(Direction.Left, 90)
-  loopCounter = 4
+  // variables
+  loopCounter1 = 4
+  // goes from lower left corner to upper right corner
   while (loopCounter >= 0) {
     basic.pause(500)
-    sprite.move(-1)
+    sprite.set(LedSpriteProperty.Y, loopCounter)
     loopCounter = loopCounter - 1
+    // goes from right to left
+    while (loopCounter1 >= 0) {
+      basic.pause(500)
+      sprite.set(LedSpriteProperty.X, loopCounter1)
+      loopCounter1 = loopCounter1 - 1
+    }
   }
-  loopCounter = 4
-  sprite.turn(Direction.Right, 90)
-  while (loopCounter >= 0) {
-    basic.pause(500)
-    sprite.move(-1)
-    loopCounter = loopCounter - 1
-  }
+
   sprite.delete()
-  basic.pause(500)
   basic.showIcon(IconNames.Happy)
 })
